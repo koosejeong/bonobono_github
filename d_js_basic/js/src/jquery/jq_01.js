bfba012558d38cbabd8d0ecd2549e7e5903a3561
koosejeong <pcpooh30@gmail.com>
2019-10-16 17:48:39 +0900

    jquery basic, event - css document

--- d_js_basic/js/src/jquery/jq_01.js
+++ d_js_basic/js/src/jquery/jq_01.js
@@ -5,7 +5,7 @@
   $('#wrap').css({'width':'700px', 'height':'auto', 'backgroundColor':'#faa'});
   $('h1').css({'width':'100%', 'height':'auto', 'backgroundColor':'#acf'});
 
-  //h2.addEventListener('click;, function(){});
+  //h2.addEventListener('click', function(){});
   $('h2').on('click', function(){
     $('#wrap').animate({'height':'1000px','backgroundColor':'#0af'});
   });
