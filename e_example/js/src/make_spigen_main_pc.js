741f0bd03d804d32d156e735ea66e2e8fee05e6f
koosejeong <pcpooh30@gmail.com>
2019-10-18 17:32:25 +0900

    jQuery function basic and css and html

--- e_example/js/src/make_spigen_main_pc.js
+++ e_example/js/src/make_spigen_main_pc.js
@@ -18,19 +18,52 @@
   //----------------------------
   //버튼 클릭 시 side_gnb 나타나게 만들기
   const openGnbBtn = $('.gnb_btn>button');
-  const closeGnbBtn = $('.close_gnb_btn');
+  const closeGnbBtn = $('.close_gnb_btn>button');
   const sideGnb = $('.side_gnb');
   let timed = 500;
 
-  openGnbBtn.on('click', function(){
-    //sideGnb.show();
+  openGnbBtn.on('click', function(e){
+    e.preventDefault();
     sideGnb.fadeIn(timed/2); //500/2
+    //sideGnb.show();
     
   });
-  closeGnbBtn.on('click', function(){
-    //sideGnb.hide();
+  closeGnbBtn.on('click', function(e){
+    e.preventDefault();
     sideGnb.fadeOut(timed);
+    //sideGnb.hide();
+  });
+  //파라미터.preventDefault(); 
+  //ㄴ내가 모르는 어떤 click 이벤트가 내장 되어 있을지 모르기 때문에 기본 이벤트 지워주고 시작. 
+
+  //------------------------------
+  //gnb에 마우스 올리면 dd 나오게 만들기.
+
+  const gnbDd = gnb.find('dd');
+
+  gnb.on('mouseenter', function(){
+    gnbDd.stop().slideDown();
+  });
+  gnb.on('mouseleave', function(){
+    gnbDd.stop().slideUp();
   });
 
+  //------------------------------
+  //gnb>dt에 focus 시 dd가 나타나게 만들기.
+
+  const gnbDt = gnb.find('dt');
+  const gnbTitleLink = gnbDt.children('a');
+  const gnbListLink = gnbDd.children('a');
+
+  gnbTitleLink.on('focus', function(){
+    gnbDd.stop().slideDown();
+  });
+  gnbListLink.eq(-1).on('blur', function(){
+    gnbDd.stop().slideUp();
+  });
+  
 
 })(jQuery);
+
+//focus가 가능한 요소
+//a, button, form(input, textarea, select)
