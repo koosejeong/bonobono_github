d6630011f4c6cca53592d56dc8c50a34900f74e3
koosejeong <pcpooh30@gmail.com>
2019-10-29 14:32:34 +0900

    delete files

--- f_new_example/public/js/src/temp/slide_01.js
+++ f_new_example/public/js/src/temp/slide_01.js
@@ -9,22 +9,58 @@
   const indicator = viewBox.find('.view_indicator');
   const indiLi =  indicator.find('li');
   const indiLink = indiLi.children('a');
-  //onst slideForm = viewBox.find('.slide_form');
-  const guideBox = viewBox.find('.guide');
+  const slideForm = viewBox.find('.slide_form');
+  const guideBox = slideForm.find('.guide');
+  const slideEach = guideBox.children('div');
 
-  //indicator.css({ 'zIndex':500});
-  //guideBox.css({ 'position':'relative', 'top':0, 'left':0});
+  let go;
+  let myn = 0;
+  let maxn = slideEach.length;
+  let timed = 500;
+
+  const MoveSlide = function(n){
+      indiLink.removeClass('action'); //모든 a의 action 클래스 지우기
+      indiLi.eq(n).children('a').addClass('action'); //선택된 a에 action 클래스 주기
+      guideBox.animate({'marginLeft':(-100 * n) + '%'});
+    }
+    MoveSlide(0);//최초 호출
+// ==================================
 
   indiLink.on('click focus', function(evt){
     evt.preventDefault();
-    let i = $(this).parent('li').index();
-    //순서를 가지고 있는 것은 li이기 때문에, a의 부모인 li에게 index()
-    //console.log(i);
+    let a = $(this).parent('li').index();
+    myn = a;
+    // //순서를 가지고 있는 것은 li이기 때문에, a의 부모인 li에게 index()
+    // //console.log(i);
 
-    indiLink.removeClass('action');
-    $(this).addClass('action');
-    guideBox.stop().animate({'left':-100 * i+'%'});
-
+    // indiLink.removeClass('action');9
+    // $(this).addClass('action');
+    // guideBox.stop().animate({'left':-100 * i+'%'});
+  
+    MoveSlide(myn);
+    
   });
+
+  
+
+  // 베너 움직이기=================================
+
+  const goSlide = function(){
+    go = setInterval(function(){
+    myn++;
+    if( myn >= maxn ){
+      myn = 0; //가지고 있는 배너의 수를 넘기지 않도록 초기화 
+    }
+    MoveSlide(myn); 
+    }, timed * 4);
+  } //goSlide
+  
+  const StopSlide = function(){
+    clearInterval(go);
+  };
+  
+  goSlide();
+
+  viewBox.on({'mouseleave': goSlide, 'mouseenter': StopSlide }); 
   
 })(jQuery);
--- f_new_example/public/js/src/temp/slide_02.js
+++ f_new_example/public/js/src/temp/slide_02.js
@@ -4,49 +4,109 @@
   const indiLi = indicator.find('li');
   const indiLiLink = indiLi.children('a');
 
+  const viewBtn = viewBox2.find('.view_btn');
+  const play = viewBtn.find('.play');
+  const pause = viewBtn.find('.pause');
+
   const slideForm = viewBox2.find('.slide_02_form');
   const slideGuide = slideForm.children('.guide');
   const slideEach = slideGuide.children('.banner_area');
 
   let timed = 500;
-
+  let myn = 0; //시작점이 될 임의의 변수 선언(on click과 setInterval에서 동시에 사용할 카운트값)
+  let maxn = slideEach.length;
+  let go;
 
   // ----------------------------------------------------
 
-  slideEach.eq(0).addClass('action');
-  indiLi.eq(0).children(indiLiLink).addClass('action');
+  //slideEach.eq(0).addClass('action');
+  //indiLi.eq(0).children(indiLiLink).addClass('action');
 
-  indiLiLink.on('click', function(e){
+
+  //공통 수행 영역 -----------------------------------------------
+  const MoveSlide = function(n){
+    indiLiLink.removeClass('action'); //모든 a의 action 클래스 지우기
+    indiLi.eq(n).children('a').addClass('action'); //선택된 a에 action 클래스 주기
+    slideGuide.animate({'marginLeft':(-100 * n) + '%'}, function(){
+      slideEach.removeClass('action'); // 액션 효과 지우기
+      slideEach.eq(n).addClass('action'); //n번째 광고(div)에만 action 효과 주기
+      
+      setTimeout( function(){
+        slideEach.eq(n).addClass('action');
+      }, timed); //0.5초뒤에 action 효과 실행
+    });
+  }
+  MoveSlide(0);
+
+  //일정시간 마다 배너 움직이기-------------------------------------------
+
+  const goSlide = function(){
+    go = setInterval(function(){
+    myn++;
+    if( myn >= maxn ){
+      myn = 0; //가지고 있는 배너의 수를 넘기지 않도록 초기화 
+    }
+    MoveSlide(myn); 
+    }, timed * 4);
+  } //goSlide
+  
+  const StopSlide = function(){
+    clearInterval(go)
+  };
+  
+  goSlide();
+
+  //viewBox2.on({'mouseleave': goSlide, 'mouseenter': StopSlide }); 
+  
+  // ----------------------------------------------------------------------
+  play.hide();
+  
+  play.on('click', function(){
+    goSlide();
+    $(this).hide();
+    $(this).siblings().show();
+  });
+  pause.on('click', function(){
+    StopSlide();
+    $(this).hide();
+    $(this).siblings().show();
+  });
+
+  //클릭하거나 포커스 잡히면 배너 멈추게 하기------------------------------------------
+
+  indiLiLink.on('click focus', function(e){
     e.preventDefault();
+    StopSlide();
     let myThis = $(this);
     let myThisPar = myThis.parent('li');
     let i = myThisPar.index();
+    myn = i;
 
-    indiLiLink.removeClass('action');
-    myThis.addClass('action');
-    slideGuide.animate({'marginLeft':(-100 * i) + '%'}, function(){
-      slideEach.removeClass('action');
-      slideEach.eq(i).addClass('action');
-      
-      setTimeout( function(){
-        slideEach.eq(i).addClass('action');
-      }, timed );
-    });
+    MoveSlide(myn);
   });
+  
 
-  //변수 i는 외부에서 공용으로 사용할 수 있도록, 전역 변수로 선언해야 한다.
-  //인디케이터, 광고배너 이동 후 처리하는 방법은 별도의 함수 처리. (let i=myThis... ~ setTimeout() 까지 한데 묶어서 함수화하여 처리.)
-  //setInterval(), clearInterval()
+  //---------------------------------------------------------------
+  /* //trigger()를 이용한 광고 슬라이드 효과 주기
+  let i = 0;
+  setInterval(function(){
+    i++;
+    indiLiLink.eq(i).trigger('click');
 
-  let go;
-  const Goslide = () =>{
-    go =  setInterval(function(){ /* 수행할 기능*/ }, timed*5)
-  }
-  const stopSlide = clearInterval(go);
-  viewBox2.on('mouseenter', stopSlide);
-  viewBox2.on('mouseleave', goSlide);
-
+  }, timed * 3);
+  //=========================================================
+  
+  */
 
 })(jQuery);
 
-//delay - animation 처리를 하는 메소드 앞에만 사용할 수 있다. 
+  //delay() - animation 처리를 하는 메소드 앞에만 사용할 수 있다. 
+  //setTimeout() : 일정 시간 뒤에 수행
+  //setInterval() : 일정 시간마다 수행
+  //clearInterval() : setInterval()의 기능을 취소/정지
+
+  
+
+
+
+
