68c25fe8611d8b982c9a3ccd62a48ce6207a8322
koosejeong <pcpooh30@gamil.com>
2019-11-08 17:45:26 +0900

    new indicator and slide banner cording

--- f_new_example/public/js/src/temp/indicator_test_01.js
+++ f_new_example/public/js/src/temp/indicator_test_01.js
@@ -5,24 +5,56 @@
   const indicator = minoProdut.children('.indicator');
   const indiLi = indicator.children('li');
   const indiLink = indiLi.children('a');
+  let indiLiLen = indiLi.length;
   const adverBanner = minoProdut.find('.product').children('ul');
   const adverNth = adverBanner.children('li');
 
+  adverNth.find('a').attr({'tabIndex':-1}); //tabIndex의 default는 0
   indiLi.eq(0).addClass('active');
   // --------------------------------------------------------
-
-  indiLink.on('focus', function(e){
+  let i = 0;
+  indiLink.on('focus click', function(e){
     e.preventDefault();
-    let i = $(this).parent('li').index();
+    i = $(this).parent('li').index();
     adverBanner.animate({ 'marginLeft':(-100 * i) + '%'});
 
     indiLi.eq(i).addClass('active');
     indiLi.eq(i).siblings('li').removeClass('active');
 
-    $(this).on('click', function(e){
+    $(this).on('keyup', function(e){ //키보드 제어
       e.preventDefault();
-      adverNth.eq(i).children('a').focus();
+      //console.log(e.keyCode);
+      if( e.keyCode === 13 ) { //엔터치면 해당 되는 광고로 포커스 가게 처리
+        adverNth.eq(i).children('a').focus();
+      }
     });
-  });
+
+  }); // indiLink
+
+  // ---------------------------------------
+    let go, timed = 1000; 
+    const slideGo = function(){
+      go = setInterval( function(){
+        i++;
+        if( i >= indiLiLen ){ 
+          i = 0;
+        } 
+        adverBanner.animate({ 'marginLeft':(-100 * i) + '%'});
+        indiLi.eq(i).addClass('active');
+        indiLi.eq(i).siblings('li').removeClass('active');
+      }, timed );
+    };
+    const slideStop = function(){
+      clearInterval(go);
+    };
+    slideGo(); //최초 호출
+
+    minoProdut.on('mouseenter', function(){
+      slideStop();
+    });
+    minoProdut.on('mouseleave', function(){
+      slideGo();
+    });
+ 
 
 })(jQuery);
--- test/js/src/test022.js
+++ test/js/src/test022.js
@@ -1,54 +1,122 @@
 // test022.js
 ( function($){
 
+  // 변수 선언-------------------------------
   const miniProGuide = $('.miniProduct');
-  const indi = miniProGuide.children('.indicator');
-  const indiLi = indi.children('li');
-  const indiBtn = indiLi.children('button');
+  const miniProGuide02 = $('.miniProduct02');
+  const miniProGuide03 = $('.miniProduct03');
+
+  const btnArea = miniProGuide.children('.btn');
+  const btnArea02 = miniProGuide02.children('.btn');
+  const btnArea03 = miniProGuide03.children('.btn');
+
+  const indiBtn = btnArea.children('button');
+  const indiBtn02 = btnArea02.children('button');
+  const indiBtn03 = btnArea03.children('button');
+
   const miniProduct = miniProGuide.children('.product');
-  const miniLi = miniProduct.children('li');
+  const miniProduct02 = miniProGuide02.children('.product');
+  const miniProduct03 = miniProGuide03.children('.product');
+
+  const miniUl = miniProduct.children('ul');
+  const miniUl02 = miniProduct02.children('ul');
+  const miniUl03 = miniProduct03.children('ul');
+
+  let miniLi = miniUl.children('li');
+  let miniLi02 = miniUl02.children('li');
+  let miniLi03 = miniUl03.children('li');
+
   const url = "../img/";
   const productImg = "slide_0";
 
-  
   let liLen = miniLi.length;
+  let myN = 0;
+
+  // 이미지 불러오기------------------------------------
   for( let i=0; i <= liLen; i++ ){
     miniLi.eq(i).css({'backgroundImage':`url("${url+productImg+(i+1)}.png")`});
+    miniLi02.eq(i).css({'backgroundImage':`url("${url+productImg+(i+1)}.png")`});
+    miniLi03.eq(i).css({'backgroundImage':`url("${url+productImg+(i+1)}.png")`});
   }
-  
-  //miniLi.eq(-1).clone(true).prependTo(miniProduct);
- const cssMv = function(myN){
-  miniProduct.animate({ 'left':-100 * myN + '%'});
- }
 
+  // 마지막 요소 복제-----------------------------------
+  miniLi.eq(-1).clone(true).prependTo(miniUl);
+  miniLi02.eq(-1).clone(true).prependTo(miniUl02);
+  miniLi03.eq(-1).clone(true).prependTo(miniUl03);
+
+  miniLi = miniUl.children('li');
+  liLen = miniLi.length; //4
+
+  // miniProduct01 영역-------------------------------
 
   indiBtn.on('click', function(e){
     e.preventDefault();
-    //console.log(indiBtn);
-    //console.log(myN);
-    let myN = $(this).parent('li').index();
-    myN++;
-    if( myN >= liLen ){
-      myN = 0;
-    } else if( myN <= 0 ){
+    if( $(this).index() === 0 ){ 
+      myN++;
+       if( myN >= liLen-1 ){
+        myN = 0;
+      }
+    } else if($(this).index() === 1 ) { 
       myN--;
+       if( myN <= -1 ){
+         miniUl.css({ 'left':0});
+         myN = liLen-2;
+       }
     }
-    cssMv(myN); 
-    /*
-      let myN = $(this).parent('li').index();
-      switch(myN) {
-        case  0:
-          cssMv(myN);
-        break;
-        case 1:
-          cssMv(myN);
-        break;
-        case 2:
-          cssMv(myN);
-        break;
-      }*/
+    miniUl.css({ 'left':-100 * myN + '%'});
+    
     $(this).addClass('action');
-    $(this).parent('li').siblings('li').children('button').removeClass('action');
+    $(this).siblings('button').removeClass('action');
+
+  });
+
+  // miniProduct02 영역-------------------------------
+  miniLi02.eq(0).siblings().css({ 'display':'none'});
+
+  indiBtn02.on('click', function(e){
+    e.preventDefault();
+    if( $(this).index() === 0 ){ 
+      myN++;
+
+       if( myN >= liLen-1 ){
+        myN = 0;
+      }
+    } else if($(this).index() === 1 ) { 
+      myN--;
+       if( myN <= -1 ){
+         miniUl02.css({ 'left':0});
+         myN = liLen-2;
+       }
+    }
+    miniLi02.eq(myN).css({ zIndex: 10 }).stop().fadeIn();
+      miniLi02.eq(myN).siblings().hide(0, function(){
+      $(this).css({zIndex:0});
+      });
+    
+    $(this).addClass('action');
+    $(this).siblings('button').removeClass('action');
+  });
+
+  // miniProduct03 영역-------------------------------
+
+  indiBtn03.on('click', function(e){
+    e.preventDefault();
+    if( $(this).index() === 0 ){ 
+      myN++;
+
+       if( myN >= liLen-1 ){
+        myN = 0;
+      }
+    } else if($(this).index() === 1 ) { 
+      myN--;
+       if( myN <= -1 ){
+         miniUl03.css({ 'top':'100%'});
+         myN = liLen-2;
+       }
+    }
+    miniUl03.animate({ 'top':-100 * myN + '%'});
+    $(this).addClass('action');
+    $(this).siblings('button').removeClass('action');
   });
 
 })(jQuery);
